ccac63a712b9f0082dd9096e08884bb3172557fa
David Calhoun <dpcalhoun@gmail.com>
2014-05-05 07:45:09 -0500

    Update README file ending to match Yeoman recipe.

--- app/index.js
+++ app/index.js
@@ -209,7 +209,7 @@ PlaybookGenerator.prototype.app = function app() {
   this.template('_config.build.yml', '_config.build.yml');
   this.template('_package.json', 'package.json');
   this.template('_bower.json', 'bower.json');
-  this.template('_README.MD', 'README.md');
+  this.template('_README.md', 'README.md');
 };
 
 PlaybookGenerator.prototype.projectfiles = function projectfiles() {
