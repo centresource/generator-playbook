76c99dac7b784ca0156e7bde4ce8bb56118e4067
Pocketbird <colin@codefashioned.com>
2014-12-29 12:36:45 -0600

    Set sassComp props to convert variable to lowercase.

--- app/index.js
+++ app/index.js
@@ -101,7 +101,8 @@ PlaybookGenerator.prototype.askForTools = function askForTools() {
 
     // Multiple choice 'None' to false
     this.jsPre = props.jsPre === 'None' ? false : props.jsPre.toLowerCase();
-    this.sassComp = props.sassComp;
+    // Set sassComp variable to lowercase
+    this.sassComp = props.sassComp : props.sassComp.toLowerCase();
 
     cb();
   }.bind(this));
