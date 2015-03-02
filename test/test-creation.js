f2916e8dda8c0eab97c6d33156b94c4e78b85caa
Max Beizer <max.beizer@gmail.com>
2015-03-02 12:05:10 -0600

    Add first pass at generator tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -5,34 +5,49 @@ var path    = require('path');
 var helpers = require('yeoman-generator').test;
 
 
-describe('playbook generator', function () {
-    beforeEach(function (done) {
-        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-            if (err) {
-                return done(err);
-            }
+describe('playbook', function () {
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        return done(err);
+      }
 
-            this.app = helpers.createGenerator('playbook:app', [
-                '../../app'
-            ]);
-            done();
-        }.bind(this));
+      this.app = helpers.createGenerator('playbook:app', ['../../app']);
+
+      done();
+    }.bind(this));
+  });
+
+  it('creates expected files', function (done) {
+    var expected = [
+      'app/index.html',
+      'app/404.html',
+      'app/humans.txt',
+      'app/robots.txt',
+      '.jshintrc',
+      '.gitignore',
+      '.editorconfig',
+      '.csslintrc',
+      '.bowerrc',
+      'bower.json',
+      'Gruntfile.js',
+      'package.json'
+    ];
+
+    helpers.mockPrompt(this.app, {
+      'authorName': 'foo',
+      'authorEmail': 'bar',
+      'jsPre': 'None',
+      'sassComp': 'Ruby',
+      'googleAnalytics': false,
+      'deploy': false
     });
 
-    it('creates expected files', function (done) {
-        var expected = [
-            // add files you expect to exist here.
-            '.jshintrc',
-            '.editorconfig'
-        ];
+    this.app.options['skip-install'] = true;
 
-        helpers.mockPrompt(this.app, {
-            'someOption': true
-        });
-        this.app.options['skip-install'] = true;
-        this.app.run({}, function () {
-            helpers.assertFiles(expected);
-            done();
-        });
+    this.app.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
     });
+  });
 });
